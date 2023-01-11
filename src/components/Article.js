719e27c6350b63f5880cb80c58376a18c9c34952
chris-wamae <chriswamae123@gmail.com>
2023-01-11 15:49:20 +0300

    Created Article component in Article.js file

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,9 @@
-function Article(){
+function Article({title,date,preview}){
     return (
       <article>
-        
+        <h3>{title}</h3>
+        <small>{date}</small>
+        <p>{preview}</p>
       </article>
     )
 }
